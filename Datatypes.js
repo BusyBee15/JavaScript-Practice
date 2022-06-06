4b08b81f5dc1e645993c58294810b34815850f6e
Annapoorneswari H.P <annapoorneswarihp15@gmail.com>
2022-06-06 11:42:51 +0530

    datatypes in JS

---
+++ Datatypes.js
@@ -0,0 +1,17 @@
+const first = "abc"
+console.log(typeof first)
+
+const second = 60
+console.log(typeof second)
+
+const third = true
+console.log(typeof third)
+
+const fourth = [1,2,3]
+console.log(typeof fourth)
+
+const fifth = null;
+console.log(typeof fifth)
+
+let sixth;
+console.log(typeof sixth)
--- externalJS.js
+++ externalJS.js
@@ -1,8 +1,15 @@
 console.log("Hey")
-// console.log("Hey")
-console.log("Hey")
+
 
 let name="anu";
 console.log(name)
 
-console.log(5*2)
+// console.log(5*2)
+
+console.log('10'-'20')
+console.log('10'*'20')
+console.log('10'/'20')
+console.log('10'+'20')
+
+let str = '10'
+console.log(typeof(str))
