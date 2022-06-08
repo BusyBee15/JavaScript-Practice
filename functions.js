4fd7e41b0ea4e55580fe44dff440d58e7c63e67f
Annapoorneswari H.P <annapoorneswarihp15@gmail.com>
2022-06-08 06:44:12 +0530

    returning something from the function

--- functions.js
+++ functions.js
@@ -1,7 +1,12 @@
-function greet(name) {
-    document.write('hello '+name)
-    document.write('<br/>')
-}
+// function greet(name) {
+//     document.write('hello '+name)
+//     document.write('<br/>')
+// }
 
-greet('Anu')
-greet('Monika')
+// greet('Anu')
+// greet('Monika')
+const calculate = (value) =>{
+       return value*2
+}
+const width = calculate(100)
+console.log(width)
