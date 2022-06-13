dadb1bdef2c8a1299e3be576d31c44930ac3b54a
Annapoorneswari H.P <annapoorneswarihp15@gmail.com>
2022-06-13 20:15:27 +0530

    Math Object

---
+++ JS/MathObject.js
@@ -0,0 +1,10 @@
+
+const num = 4.5678
+let res = Math.floor(num)
+
+console.log(res)
+// ans 4
+
+console.log(Math.ceil(8.99)) ///ans 9
+console.log(Math.random()) //0.3038402117817931
+console.log(Math.floor(Math.random()*10)) //5
