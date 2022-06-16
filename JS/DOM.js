d6ce6f351a838fc7b7c840e39fc5805db3e3bb2b
Annapoorneswari H.P <annapoorneswarihp15@gmail.com>
2022-06-16 16:19:36 +0530

    basic concept of DOM,basics of window object, document object

---
+++ JS/DOM.js
@@ -0,0 +1,14 @@
+document.body.style.backgroundColor = 'pink'
+document.getElementById('btn').style.backgroundColor='violet'
+
+// console.log(window)
+
+// const btn = document.getElementById('btn')
+// console.log(btn)
+// const name = btn.nodeName
+// console.log(name)
+// const css = btn.style
+// console.log(css)
+
+console.log(document)
+console.dir(document)
