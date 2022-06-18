264846f021bc8ac6faeac3cb4fde23a123fb707b
Annapoorneswari H.P <annapoorneswarihp15@gmail.com>
2022-06-18 11:53:57 +0530

    Lear about previousSibling, previousElementSibling, nextSibling, nextElementSibling, textContext, nodeValue

--- JS/NavigateDOM3.js
+++ JS/NavigateDOM3.js
@@ -9,3 +9,12 @@ console.log(first)
 const second = first.nextSibling.nextSibling
 
 console.log(second)
+
+const last = document.querySelector('#DOM3-item2')
+console.log(last)
+
+const third = last.previousSibling.previousSibling
+console.log(third)
+
+//previousElementSibling
+console.log(first.nextElementSibling)
---
+++ JS/TextContent.js
@@ -0,0 +1,11 @@
+const item = document.getElementById('DOM2h2')
+console.log(item)
+console.log(item.nodeValue)
+
+// console.log(item.nodeValue)
+console.log(item.childNodes)
+console.log(item.firstChild.nodeValue)
+
+
+//more dirct way
+console.log(item.textContent)
