ef50f4ba134df553fab145093cde3633cc2d795e
Annapoorneswari H.P <annapoorneswarihp15@gmail.com>
2022-06-20 16:27:10 +0530

    Learn how to add elements to DOM using prepedn method and also learnt the alternative to createTextNode + appendChild

---
+++ JS/Prepend.js
@@ -0,0 +1,7 @@
+//prepend + innerText + Remove + RemoveChild
+
+const heading = document.createElement('h2')
+heading.innerText = `I'm a dynamic heading`
+document.body.prepend(heading)
+// console.log(heading)
+
