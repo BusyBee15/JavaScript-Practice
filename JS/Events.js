0cb417695135d7109b9ca883ec3fe6474e7a08b2
Annapoorneswari H.P <annapoorneswarihp15@gmail.com>
2022-06-21 17:58:59 +0530

    toggling functionality

--- JS/Events.js
+++ JS/Events.js
@@ -2,8 +2,26 @@ const btn = document.querySelector('.btn')
 // console.log(btn)
 const heading = document.querySelector('.events-h2')
 
-btn.addEventListener('click', function(){
-    // console.log('hey you clicked me') - ok cb working fine
-    //Set up the functionality
-    heading.classList.add('colors')
-})
+// function cb(){
+//     heading.classList.add('colors')
+// }
+// juts pass the above function reference as 2nd parameter to the event listener
+
+// btn.addEventListener('click', function(){
+//     // console.log('hey you clicked me') - ok cb working fine
+//     //Set up the functionality
+//     heading.classList.add('colors')
+// })
+
+function cb(){
+    let hasClass = heading.classList.contains('colors')
+    if(hasClass){
+        heading.classList.remove('colors')
+    }else{
+        heading.classList.add('colors')
+    }
+}
+
+btn.addEventListener('click',cb)
+
+
