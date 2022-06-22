0c0b6143d282e3e0cae0713085f35ab43c837cf8
Annapoorneswari H.P <annapoorneswarihp15@gmail.com>
2022-06-22 11:49:19 +0530

    targte versus currentTarget

--- JS/Event-Object.js
+++ JS/Event-Object.js
@@ -1,6 +1,8 @@
 const heading = document.querySelector('#EO-h1')
-const btn = document.querySelector('.EO-btn')
+// const btn = document.querySelector('.EO-btn')
 const link = document.querySelector('#EO-link')
+// const btn2 = document.querySelector('.EO-btn2')
+const buttons = document.querySelectorAll('.EO-btn')
 
 heading.addEventListener('click', function(event){
     // heading.classList.add('colors')
@@ -9,11 +11,31 @@ heading.addEventListener('click', function(event){
     console.log(this)
 })
 
+
+buttons.forEach(function(btn){
+    btn.addEventListener('click',function(event){
+        console.log('current target',event.currentTarget)
+        console.log('target', event.target)
+    })
+})
 // heading.addEventListener('click', (event)=>{
 //     console.log(this)
 // })
 
-link.addEventListener('click', function(event){
-   event.preventDefault()
-})
+// link.addEventListener('click', function(event){
+//    event.preventDefault()
+// })
+
+
+// target versus current target
+// btn.addEventListener('click', function(event){
+//     console.log('current target',event.currentTarget)
+//     console.log('target', event.target)
+// })
+
+// btn2.addEventListener('click', function(event){
+//     console.log('current target',event.currentTarget)
+//      console.log('target', event.target)
+// })
+
 
