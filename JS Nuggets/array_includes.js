7d358f583c2b2e127414bc4025aa363bfdd332ce
Annapoorneswari H.P <annapoorneswarihp15@gmail.com>
2022-07-01 07:04:17 +0530

    learnt about array includes method

---
+++ JS Nuggets/array_includes.js
@@ -0,0 +1,12 @@
+const groceries = ['milk','bread', 'veggies']
+
+let randomItem = 'lemon'
+randomItem = 'milk'
+
+// const isIncluded = groceries.includes(randomItem, 1)
+// console.log(isIncluded)
+
+// using in conditionals
+if(groceries.includes(randomItem)){
+console.log("yes, it is on the list");
+}
